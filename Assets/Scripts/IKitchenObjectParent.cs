105150f4b51ef5f8c8f34fb6ab7c1446b4ec6747
CocoB15 <cosmin_kkb@yahoo.com>
2023-08-11 13:18:10 +0300

    created interface still wip

---
+++ Assets/Scripts/IKitchenObjectParent.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IKitchenObjectParent 
+{
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+}
