85968376b23f34fe5d05bc84c738fa7b4ca5f4c9
CocoB15 <cosmin_kkb@yahoo.com>
2023-08-14 12:28:03 +0300

    Finished ContainerCounter
    refactred code + need help on selected visual pls help!!!!

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,50 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : BaseCounter,IKitchenObjectParent
+public class ClearCounter : BaseCounter
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSo;
-    [SerializeField] private Transform counterTopPoint;
-    private KitchenObject kitchenObject;
+    
    
     
     public override void  Interact(Player player)
     {
 
-        if (kitchenObject==null)
-        {
-            Transform kitchenObjectTransform=Instantiate(kitchenObjectSo.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-        }
-        else
-        {
-            //Give object to player
-            kitchenObject.SetKitchenObjectParent(player);
-        }
+       
     }
 
-    public Transform GetKitchenObjectFollowTransform()
-    {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(KitchenObject kitchenObject)
-    {
-        this.kitchenObject = kitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject()
-    {
-        return kitchenObject;
-    }
-
-    public void ClearKitchenObject()
-    {
-        kitchenObject = null;
-    }
-
-    public bool HasKitchenObject()
-    {
-        return kitchenObject != null;
-    }
+   
 }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,49 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ContainerCounter : BaseCounter, IKitchenObjectParent
+public class ContainerCounter : BaseCounter
 {
+    public EventHandler OnPlayerGrabbedObject;
     [SerializeField] private KitchenObjectSO kitchenObjectSo;
-    [SerializeField] private Transform counterTopPoint;
-    private KitchenObject kitchenObject;
     
-    public void Interact(Player player)
+    
+    public override void Interact(Player player)
     {
+            Transform kitchenObjectTransform=Instantiate(kitchenObjectSo.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+            OnPlayerGrabbedObject?.Invoke(this,EventArgs.Empty);
+    }
+   
 
-        if (kitchenObject==null)
-        {
-            Transform kitchenObjectTransform=Instantiate(kitchenObjectSo.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-        }
-        else
-        {
-            //Give object to player
-            kitchenObject.SetKitchenObjectParent(player);
-        }
-    }
-    public Transform GetKitchenObjectFollowTransform()
-    {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(KitchenObject kitchenObject)
-    {
-        this.kitchenObject = kitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject()
-    {
-        return kitchenObject;
-    }
-
-    public void ClearKitchenObject()
-    {
-        kitchenObject = null;
-    }
-
-    public bool HasKitchenObject()
-    {
-        return kitchenObject != null;
-    }
+  
 }
