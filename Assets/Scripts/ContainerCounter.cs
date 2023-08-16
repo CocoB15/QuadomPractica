5943a76c83ce648c67d22fe3036c92603743f3a4
CocoB15 <cosmin_kkb@yahoo.com>
2023-08-16 12:02:55 +0300

    CuttingCounter made and cutting

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -6,15 +6,14 @@ using UnityEngine;
 public class ContainerCounter : BaseCounter
 {
     public EventHandler OnPlayerGrabbedObject;
-    [SerializeField] private KitchenObjectSO kitchenObjectSo;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
     
     
     public override void Interact(Player player)
     {
         if (!player.HasKitchenObject())
         {
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+            KitchenObject.SpawnKitchenObject(kitchenObjectSO, player);
             OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
         }
     }
--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CuttingCounter : MonoBehaviour
+public class CuttingCounter : BaseCounter
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+    public override void Interact(Player player)
     {
-        
+        if (!HasKitchenObject())
+        {
+            //no KitchenObject
+            if (player.HasKitchenObject())
+            {
+                //player holding something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                //player has nothing
+            }
+        }
+        else
+        {
+            //has KitchenObject
+            if (player.HasKitchenObject())
+            {
+                //player is holding smth
+            }
+            else
+            {
+                //give player the kitchenObject
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void InteractAlternate(Player player)
     {
-        
+        if (HasKitchenObject())
+        {
+            //there is kitchenObject on there
+            GetKitchenObject().DestroySelf();
+            KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
+            
+        }
     }
 }
+
