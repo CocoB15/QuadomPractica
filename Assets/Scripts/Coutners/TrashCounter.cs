dc026cf05c5daa1c7ad736ac658a3ca0b2029659
CocoB15 <cosmin_kkb@yahoo.com>
2023-08-17 12:18:42 +0300

    This counter is trash
    like literally

--- Assets/Scripts/Coutners/TrashCounter.cs
+++ Assets/Scripts/Coutners/TrashCounter.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class TrashCounter : BaseCounter
 {
-    
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
 }
